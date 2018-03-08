9ed6976d3bd0eab13b0611ec3a635ef2781ae5fe
Yomaira Escano <yjescano@gmail.com>
2018-03-08 20:56:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,8 +3,8 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
-  # private
-  #   def song_params(*args)
-  #     params.require(:song).permit(*args)
-  #   end
+  private
+    def song_params(*args)
+      params.require(:song).permit(*args)
+    end
 end
