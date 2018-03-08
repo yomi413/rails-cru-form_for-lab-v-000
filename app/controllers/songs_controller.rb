82dd6bbfe4d155e191bb3072bd653e3c8189f8c5
Yomaira Escano <yjescano@gmail.com>
2018-03-08 21:34:45 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,6 +11,8 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
   end
 
   private
