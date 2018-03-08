914aba645807ea1fc30a8b0f02806d7d6381635e
Yomaira Escano <yjescano@gmail.com>
2018-03-08 21:28:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,7 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
     @genre = Genre.find(params[:id])
+    @artist = Artist.find(params[:id])
   end
 
   private
