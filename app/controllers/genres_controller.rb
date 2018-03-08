6c2a5279951e7962c8079a3eeb663e999eb37a12
Yomaira Escano <yjescano@gmail.com>
2018-03-08 20:16:17 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,4 +6,15 @@ class GenresController < ApplicationController
   def new
     @genre = Genre.new
   end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  private
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
+    end
 end
