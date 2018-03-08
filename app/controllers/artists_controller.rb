b6d18c25b4ad9d348fdcabb1325296c5eca7d5fb
Yomaira Escano <yjescano@gmail.com>
2018-03-08 19:06:16 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,9 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
   private
