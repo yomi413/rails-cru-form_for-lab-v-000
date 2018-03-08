e323bc95dd36de87f97f2e0734aff87356f196da
Yomaira Escano <yjescano@gmail.com>
2018-03-08 21:34:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,11 @@ class SongsController < ApplicationController
   end
 
   def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
   end
 
   private
