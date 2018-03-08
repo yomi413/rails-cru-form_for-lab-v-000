8895c0639655bd16b9aa94b2b391d16907664e76
Yomaira Escano <yjescano@gmail.com>
2018-03-08 21:50:54 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs
+  resources :songs only: [:index, :show, :new, :create, :edit, :update]
   resources :genres
   resources :artists 
   # The priority is based upon order of creation: first created -> highest priority.
