0f7b14b899b62dadf0d86bdf8bc0941fc26b8e18
Yomaira Escano <yjescano@gmail.com>
2018-03-08 20:26:34 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -17,6 +17,9 @@ class GenresController < ApplicationController
     @genre = Genre.find(params[:id])
   end
 
+  def update
+  end
+
   private
     def genre_params(*args)
       params.require(:genre).permit(*args)
