09ea6c9e39c55781bf2d4519d73c5e7b16361fb0
Yomaira Escano <yjescano@gmail.com>
2018-03-08 19:48:22 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,6 +17,12 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirecto_to artist_path(@artist)
+  end
+
   private
 
     def artist_params(*args)
